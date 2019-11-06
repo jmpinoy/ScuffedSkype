84ec8a0dbeab8aeb3117b7c69c813de9d2d4c7d7
brandonleetran <brandonleetran@icloud.com>
2019-11-05 23:44:22 -0600

    Changed styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import SignIn from './components/SignIn'
 
 function App() {
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import { TextField } from '@material-ui/core'
-import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded'
 
 function SignIn() {
     return (
         <div id="flex-parent">
             <div id="flex-child">
                 <div id="signin-head">
-                    <AccountCircleRoundedIcon fontSize="large" />
-                    <h2>Sign In</h2>
+                    <i class="fas fa-user-circle fa-3x"></i>
+                    <h1>Sign In</h1>
                 </div>
                 <div id="first-name">
                     <TextField label="First Name" />
                     <br />
-                    </div>
-                    <div id="last-name">
+                </div>
+                <div id="last-name">
                     <TextField label="Last Name" />
                 </div>
                 <br />
