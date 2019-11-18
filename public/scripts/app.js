5aabac2a3d1523f751b542439b398fcec231e937
brandonleetran <brandonleetran@icloud.com>
2019-11-17 22:11:48 -0600

    Changed design. Fixed velocity on balloon. UI is complete.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+var params = new URLSearchParams(document.location.search.substring(1));
+var name = params.get("name")
+var canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+var balloons = [];
+
 $(document).ready(function () {
-    let params = new URLSearchParams(document.location.search.substring(1));
-    let name = params.get("name")
     document.getElementsByClassName('app-head')[0].innerHTML = "hello " + name;
 
     $('.app-head').fadeIn(1000, function () {
@@ -13,127 +19,203 @@ $(document).ready(function () {
             $(this).css({ 'visibility': 'hidden', 'display': 'none' });
             $('#app-heading-container').css({ 'visibility': 'hidden', 'display': 'none' });
         })
-
     }, 100);
-
-    // fix later
-    setTimeout(function() {
-        $("#chat-container").hide().slideDown(1000);
-    }, 2500);
 });
 
-
-    
-var canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var ctx = canvas.getContext("2d");
-var balloons = [];
+function voiceChat() {
+    var type = document.getElementsByClassName('input-group')[0].children[1].children[1];
+    var message;
+    if (type.textContent == 'Join Voice')
+    {
+        type.textContent = 'Disconnect';
+        message = 'Hello ' + name;
+    } 
+    else {
+        type.textContent = 'Join Voice';
+        message = 'Bye ' + name;
+    }
+    balloons.push(new Balloon(message));
+}
 
 function pushBalloon() {
     var input = document.getElementsByClassName('input-group')[0].children[0];
     balloons.push(new Balloon(input.value));
-    drawBalloon();
 }
 
 function chunkSubstr(str, size) {
-  const numChunks = Math.ceil(str.length / size)
-  const chunks = new Array(numChunks)
+    const numChunks = Math.ceil(str.length / size)
+    const chunks = new Array(numChunks)
 
-  for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
-    chunks[i] = str.substr(o, size)
-  }
-
-  return chunks
+    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+        chunks[i] = str.substr(o, size)
+    }
+    return chunks
 }
 
-
-
 function Balloon(message) {
-  this.update = function() {
-    this.x += this.velocityX;
-    this.y += this.velocityY;
-    
-    var overlapLeft = this.x - this.radius;
-    var overlapRight = this.x + this.radius;
+    this.update = function () {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
 
-    if (this.y + 275 < 0) {
-      for (var i = 0; i < balloons.length; i++)
-      {
-        if (balloons[i].y == this.y && balloons[i].x == this.x)
-        {
-          balloons.splice(balloons[i], 1);
-          return;
+        var overlapLeft = this.x - this.radius;
+        var overlapRight = this.x + this.radius;
+
+        if (this.y + 275 < 0) {
+            for (var i = 0; i < balloons.length; i++) {
+                if (balloons[i].y == this.y && balloons[i].x == this.x) {
+                    balloons.splice(balloons[i], 1);
+                    console.log(balloons.length);
+                    return;
+                }
+            }
         }
-      }
-    }
-    
-       if (overlapLeft < 0) {
-      this.velocityX = -this.velocityX+1;
-    }
-    
-    else if (overlapRight > canvas.width) {
-      this.velocityX = -this.velocityX-1;
-    }
-    
-    var addVelX = this.movingDir === 'left' ? -Math.random() / 5 : Math.random() / 5;
-    
-    if (this.velocityX > 5) {
-      this.movingDir = 'left';
-    }
-    else if (this.velocityX < -5) {
-      this.movingDir = 'right';
+
+        if (overlapLeft < 0) {
+            this.velocityX = -this.velocityX + 1;
+        }
+
+        else if (overlapRight > canvas.width) {
+            this.velocityX = -this.velocityX - 1;
+        }
+
+        var addVelX = this.movingDir === 'left' ? -Math.random() / 5 : Math.random() / 5;
+
+        if (this.velocityX > 3) {
+            this.movingDir = 'left';
+        }
+        else if (this.velocityX < -3) {
+            this.movingDir = 'right';
+        }
+
+        this.velocityX += addVelX;
     }
 
-    
-    this.velocityX += addVelX;
-  }
-  
-  this.message = chunkSubstr(message,20);
-  this.radius = 50;
-  this.movingDir = Math.round(Math.random()) ? "left" : "right";
-  this.x = Math.random() * ((canvas.width - this.radius) - this.radius) + this.radius;
-  this.y = canvas.height + 100;
-  this.velocityX = 0;
-  this.velocityY = -5;
-  this.color = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+    this.message = chunkSubstr(message, 6);
+    this.radius = 50;
+    this.movingDir = Math.round(Math.random()) ? "left" : "right";
+    this.x = Math.random() * ((canvas.width - this.radius) - this.radius) + this.radius;
+    this.y = canvas.height + 100;
+    this.velocityX = 0;
+    this.velocityY = -1;
+    this.color = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
 }
 
 function drawBalloon() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-        ctx.shadowBlur = 20;
-        ctx.shadowColor = "rgba(0,0,0,.75)";
-  
-  for (var i=0; i<balloons.length; i++) {
-    balloons[i].update();
-    
-    if (balloons[i]) {
-      ctx.beginPath();
-      ctx.ellipse(balloons[i].x, balloons[i].y, balloons[i].radius, 75, 0, 0, 2 * Math.PI);
-      ctx.moveTo(balloons[i].x, balloons[i].y+75);
-  //    ctx.lineTo(balloons[i].x+Math.random()*40-20, balloons[i].y+100);
-  //    ctx.lineTo(balloons[i].x+Math.random()*60-30, balloons[i].y+125);
-  //    ctx.lineTo(balloons[i].x+Math.random()*80-40, balloons[i].y+150);
-  //    ctx.lineTo(balloons[i].x+Math.random()*100-50, balloons[i].y+175);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      ctx.lineTo(balloons[i].x, balloons[i].y+200);
-      ctx.stroke();
-      
-      ctx.fillStyle = balloons[i].color;
-      ctx.fill();
+    for (var i = 0; i < balloons.length; i++) {
+        balloons[i].update();
+        if (balloons[i]) {
+            var centerX = balloons[i].x;
+            var centerY = balloons[i].y;
+            var radius = balloons[i].radius;
+            
+            var handleLength = (4 * (Math.sqrt(2) - 1))/3 * radius;
+            
+            var widthDiff = (radius * 0.0333);
+            var heightDiff = (radius * .4);
+            
+            var balloonBottomY = centerY + radius + heightDiff;
+            
+            // Begin balloon path
+            
+            ctx.beginPath();
+        
+            // Top Left Curve
+            
+            var topLeftCurveStartX = centerX - radius;
+            var topLeftCurveStartY = centerY;
+            
+            var topLeftCurveEndX = centerX;
+            var topLeftCurveEndY = centerY - radius;
+            
+            ctx.moveTo(topLeftCurveStartX, topLeftCurveStartY);
+            ctx.bezierCurveTo(topLeftCurveStartX, topLeftCurveStartY - handleLength - widthDiff,
+                                    topLeftCurveEndX - handleLength, topLeftCurveEndY,
+                                    topLeftCurveEndX, topLeftCurveEndY);
+                                    
+            // Top Right Curve
+            
+            var topRightCurveStartX = centerX;
+            var topRightCurveStartY = centerY - radius;
+            
+            var topRightCurveEndX = centerX + radius;
+            var topRightCurveEndY = centerY;
+            
+            ctx.bezierCurveTo(topRightCurveStartX + handleLength + widthDiff, topRightCurveStartY,
+                                    topRightCurveEndX, topRightCurveEndY - handleLength,
+                                    topRightCurveEndX, topRightCurveEndY);
+                                                
+            // Bottom Right Curve
+            
+            var bottomRightCurveStartX = centerX + radius;
+            var bottomRightCurveStartY = centerY;
+            
+            var bottomRightCurveEndX = centerX;
+            var bottomRightCurveEndY = balloonBottomY;
+            
+            ctx.bezierCurveTo(bottomRightCurveStartX, bottomRightCurveStartY + handleLength,
+                                    bottomRightCurveEndX + handleLength, bottomRightCurveEndY,
+                                    bottomRightCurveEndX, bottomRightCurveEndY);							
+            
+            // Bottom Left Curve
+            
+            var bottomLeftCurveStartX = centerX;
+            var bottomLeftCurveStartY = balloonBottomY;
+            
+            var bottomLeftCurveEndX = centerX - radius;
+            var bottomLeftCurveEndY = centerY;
+            
+            ctx.bezierCurveTo(bottomLeftCurveStartX - handleLength, bottomLeftCurveStartY,
+                                    bottomLeftCurveEndX, bottomLeftCurveEndY + handleLength,
+                                    bottomLeftCurveEndX, bottomLeftCurveEndY);
+            
+            // Create balloon color
+            ctx.fillStyle = balloons[i].color;
+            ctx.fill();                      
+            
+            // Create balloon tie
+            
+            var halfTieWidth = (radius * .12)/2;
+            var tieHeight = (radius * .12);
+            var tieCurveHeight = (radius * .13);
+            
+            ctx.beginPath();
+            ctx.moveTo(centerX - 1, balloonBottomY);
+            ctx.lineTo(centerX - halfTieWidth, balloonBottomY + tieHeight);
+            ctx.quadraticCurveTo(centerX, balloonBottomY + tieCurveHeight,
+                                        centerX + halfTieWidth, balloonBottomY + tieHeight);
+            ctx.lineTo(centerX + 1, balloonBottomY);
+            ctx.fill();
 
-      ctx.textAlign = "center";
-      ctx.fillStyle = "white";
 
-      for (var j=0; j<balloons[i].message.length; j++) {
-        ctx.fillText(balloons[i].message[j], balloons[i].x, (balloons[i].y - 25) + j*20);          
-      }
+            // Create balloon text color
+            var letterSpacing = 25;
+            var textHeight = 10;
+
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.font = "20px Arial";
+
+            for (var j = 0; j < balloons[i].message.length; j++) {
+                ctx.fillText(balloons[i].message[j], balloons[i].x, (balloons[i].y - textHeight) + j * letterSpacing);
+            }
+
+            // OLD DESIGN
+            // ctx.ellipse(balloons[i].x, balloons[i].y, balloons[i].radius, 75, 0, 0, 2 * Math.PI);
+            // ctx.moveTo(balloons[i].x, balloons[i].y + 75);
+            // ctx.lineTo(balloons[i].x+Math.random()*40-20, balloons[i].y+100);
+            // ctx.lineTo(balloons[i].x+Math.random()*60-30, balloons[i].y+125);
+            // ctx.lineTo(balloons[i].x+Math.random()*80-40, balloons[i].y+150);
+            // ctx.lineTo(balloons[i].x+Math.random()*100-50, balloons[i].y+175);
+            // ctx.lineTo(balloons[i].x, balloons[i].y + 200);
+            // ctx.stroke();
+            // ctx.fillStyle = balloons[i].color;
+            // ctx.fill();
+            // ctx.fillStyle = "white";
+        }
     }
-   }
-  
-
-  requestAnimationFrame(drawBalloon);
+    requestAnimationFrame(drawBalloon);
 }
 
-
+drawBalloon();
